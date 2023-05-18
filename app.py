4e863db604020942ac5048b3e48a04ecbc037699
aschuma <arno.schumacher@avono.de>
2023-05-18 14:26:22 +0200

    Reverted port to 5000

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ limit = int(getenv("VVS_LIMIT", 10))
 check_time_offset = int(getenv("VVS_TIME_OFFSET_MINUTES", 12))
 
 print(f"\n\n")
-print(f"----- VVS Direct Connect ------------------------------")
+print(f"----- VVS Direct Connect -----------------------------------")
 print(f"Copyright (c) 2021-2023 aschuma (https://github.com/aschuma)")
-print(f"-------------------------------------------------------")
+print(f"------------------------------------------------------------")
 print(f"\nSettings:")
 print(f"\t- VVS_FROM={from_id}\n\t- VVS_TO={to_id}\n\t- VVS_LIMIT={limit}\n\t- VVS_TIME_OFFSET_MINUTES={check_time_offset}\n\n")
 
@@ -76,4 +76,4 @@ def after_request_func(data):
 
 
 if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=5001)
+    serve(app, host="0.0.0.0", port=5000)
